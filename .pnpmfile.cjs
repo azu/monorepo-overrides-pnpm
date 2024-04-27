be8fe8f1cff06cb441a34b3cacd33d94de2ed766
azu <azuciao@gmail.com>
2024-04-27 22:26:18 +0900

    Add pnpmfile

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -6,28 +6,31 @@ function readPackage(pkg, context) {
         ["dependencies", "peerDependencies", "devDependencies"].forEach((depType) => {
             const deps = pkg[depType]
             if (deps) {
-                Object.keys(deps).forEach((dep) => {
-                    if (!rootPkg.pnpm.overrides[dep]) {
-                        throw new Error(`The package "${dep}" is not defined in the root package.json
+                Object.entries(deps).forEach(([depName, depVersion]) => {
+                    if (!rootPkg.pnpm.overrides[depName]) {
+                        throw new Error(`The package "${depName}" is not defined in the root package.json
+                        
+package.name: ${pkg.name}
 
 # How to fix?
 
-1. Add the "${dep}" to the root package.json's ${depType}
-2. Add the "${dep}" to the root package.json's pnpm.overrides
-2. Change the "${dep}" version to "*" on each package.json file
-3. pnpm install
+\`\`\`
+npm pkg set '${depType}.${depName}'='${depVersion}'
+npm pkg set 'pnpm.overrides.${depName}'='\$${depName}'
+pnpm install
+\`\`\`
 
 `)
                     }
                 })
             }
-        })
+        });
     }
     // if the pkg is root, remove dependencies/peerDependencies/devDependencies
     if (pkg.name === rootPkg.name) {
-        ["dependencies", "peerDependencies", "devDependencies"].forEach((depType) => {
-            delete pkg[depType]
-        })
+        // ["dependencies", "peerDependencies", "devDependencies"].forEach((depType) => {
+        //     delete pkg[depType]
+        // })
     }
     return pkg
 }
